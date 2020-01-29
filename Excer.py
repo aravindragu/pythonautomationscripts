c492e2cf037a68959ee8f89d9ecbb237a9dec3d9
Aravindan <aravindragu@hotmail.com>
2020-01-29 11:53:47 +0530

    Excersie file [ teting varaible , array,list,dict ]

---
+++ Excer.py
@@ -0,0 +1,40 @@
+
+print("Hello")
+
+a = 100
+b = 100.6767
+c = "Aravindan"
+
+print(a);print(b);print(c)
+
+array1=[100,200,300,700]   ## Same Data Type
+print(array1)
+
+list1=["aravindan",100.434,73]  ## List Contains Different Datatype
+print(list1)
+list1.append(10000)
+list1.insert(4,99)
+print(list1)
+
+list1.reverse()
+print(list1)
+
+tuple1= (45,"ashok","bala")  ## Read only , cant update
+print(tuple1)
+
+test = tuple1[1]
+print(test)
+
+dict1={'name':'ashok','state':'TN','country':'IN','phone':'044'}
+print(dict1)
+
+dict1['email']="ashok@gmail.com"
+print(dict1)
+
+dict1.get("keys")
+print(dict1)
+
+res = "phone" in dict1
+print(res)
+
+
